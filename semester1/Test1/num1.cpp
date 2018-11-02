a45f26e8ab6472c13b20e4b0e2b513ba1d78dac1
Artem Chernikov <artem_chernikov00@list.ru>
2018-11-02 16:46:17 +0300

    Added task test1.1, 2nd guess

--- semester1/Test1/num1.cpp
+++ semester1/Test1/num1.cpp
@@ -1,101 +1,67 @@
 #include <iostream>
 using namespace std;
 
-void enterArr(int arr[], int size);
-void sort(int arr1[], int size1, int arr2[], int size2, int first1, int first2, int arr[], int &k);
-void printArr(int arr[], int size);
-
 int main()
 {
-    int size1 = 0;
-    cout << "Enter the first size\n";
-    cin >> size1;
+    cout << "Enter the length of your string" << endl;
+    int length = 0;
+    cin >> length;
 
-    int *arr1 = new int[size1] {};
-    enterArr(arr1, size1);
+    cout << "Enter the string" << endl;
+    char *inputLine = new char[length] {};
+    for (int i = 0; i <= length; i++)
+    {
+        char currentSymbol = '\0';
+        scanf("%c", &currentSymbol);
+        if (i > 0)
+        {
+            inputLine[i - 1] = currentSymbol;
+        }
+    }
 
-    int size2 = 0;
-    cout << "Enter the second size\n";
-    cin >> size2;
+    cout << "Enter the necessary length" << endl;
+    int necessaryLength = 0;
+    cin >> necessaryLength;
 
-    int *arr2 = new int[size2] {};
-    enterArr(arr2, size2);
+    int spaceAmount = 0;
+    for (int i = 1; i + 1 < length; i++)
+    {
+        if (inputLine[i] == ' ' && isalpha(inputLine[i - 1]))
+        {
+            spaceAmount++;
+        }
+    }
 
-    int *arr = new int[size1 + size2] {};
-    int k = 0;
+    int minimumAmountOfSpaces = (necessaryLength - length) / spaceAmount;
+    int extraAmountOfSpaces = (necessaryLength - length) % spaceAmount;
 
-    sort(arr1, size1, arr2, size2, 0, 0, arr, k);
+    for (int i = 0; i < length; i++)
+    {
+        if (inputLine[i] == ' ' && isalpha(inputLine[i - 1]))
+        {
+            for (int j = 0; j <= minimumAmountOfSpaces; j++)
+            {
+                cout << ' ';
+            }
 
-    cout << "The first array: ";
-    printArr(arr1, size1);
+            if (extraAmountOfSpaces > 0)
+            {
+                cout << ' ';
+                extraAmountOfSpaces--;
+            }
 
-    cout << "The second array: ";
-    printArr(arr2, size2);
+            while (!isalpha(inputLine[i + 1]))
+            {
+                i++;
+            }
+        }
+        else
+        {
+            cout << inputLine[i];
+        }
+    }
 
-    cout << "Merged arrays: ";
-    printArr(arr, size1 + size2);
-
-    delete []arr1;
-    delete []arr2;
-    delete []arr;
-
-    cout << "Difficulty is O(n)";
+    delete[] inputLine;
 
     return 0;
-}
-
-void enterArr(int arr[], int size)
-{
-    for (int i = 0; i < size; i++)
-    {
-        cout << "Enter the " << i + 1 << " element\n";
-        cin >> arr[i];
-    }
-}
-
-void sort(int arr1[], int size1, int arr2[], int size2, int first1, int first2, int arr[], int &k)
-{
-    if (first1 == size1)
-    {
-        for (int i = first2; i < size2; i++)
-        {
-            arr[k] = arr2[i];
-            k++;
-        }
-
-        return;
-    }
-
-    if (first2 == size2)
-    {
-        for (int i = first1; i < size1; i++)
-        {
-            arr[k] = arr1[i];
-            k++;
-        }
-
-        return;
-    }
-
-    if (arr1[first1] >= arr2[first2])
-    {
-        arr[k] = arr1[first1];
-        k++;
-        sort(arr1, size1, arr2, size2, first1 + 1, first2, arr, k);
-    }
-    else
-    {
-        arr[k] = arr2[first2];
-        k++;
-        sort(arr1, size1, arr2, size2, first1, first2 + 1, arr, k);
-    }
-}
-
-void printArr(int arr[], int size)
-{
-    for (int i = 0; i < size; i++)
-    {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
 }
