db3275a71237c1faf262da6041cfbdb551a0061d
Artem Chernikov <artem_chernikov00@list.ru>
2018-11-10 19:38:12 +0300

    Added interactive mode

--- semester1/hw7/num1/num1.cpp
+++ semester1/hw7/num1/num1.cpp
@@ -6,45 +6,69 @@ int main()
 {
     Tree *tree = createTree();
 
-    printTree(tree);
-    printTreeAscending(tree);
-    printTreeDescending(tree);
+    cout << "Enter:" << endl;
+    cout << "0 - exit" << endl;
+    cout << "1 - add the value" << endl;
+    cout << "2 - remove the value" << endl;
+    cout << "3 - check if the value belongs to the set" << endl;
+    cout << "4 - print the set ascending" << endl;
+    cout << "5 - print the set descending" << endl;
+    cout << "6 - print the tree in format: (a b c)" << endl;
 
-    addElement(tree, 7);
-    addElement(tree, 4);
-    addElement(tree, 9);
-    addElement(tree, 2);
-    addElement(tree, 6);
-    addElement(tree, 8);
-    addElement(tree, 10);
-    addElement(tree, 1);
-    addElement(tree, 3);
-    addElement(tree, 5);
-
-    printTree(tree);
-    printTreeAscending(tree);
-    printTreeDescending(tree);
-
-    for (int i = 0; i < 12; i++)
+    int input = 0;
+    do
     {
-        cout << i << ' ' << (belongs(tree, i) ? "belongs" : "doesn't belong") << " to the set" << endl;
+        cin >> input;
+
+        switch (input)
+        {
+        case 1:
+        {
+            cout << "Enter the value: ";
+            int number = 0;
+            cin >> number;
+
+            addElement(tree, number);
+            cout << "The value added successfully" << endl;
+            break;
+        }
+        case 2:
+        {
+            cout << "Enter the value: ";
+            int number = 0;
+            cin >> number;
+
+            removeElement(tree, number);
+            cout << "The value removed successfully" << endl;
+            break;
+        }
+        case 3:
+        {
+            cout << "Enter the value: ";
+            int number = 0;
+            cin >> number;
+
+            cout << number << " " << (belongs(tree, number) ? "belongs" : "doesn't belong") << " to the set" << endl;
+            break;
+        }
+        case 4:
+        {
+            printTreeAscending(tree);
+            break;
+        }
+        case 5:
+        {
+            printTreeDescending(tree);
+            break;
+        }
+        case 6:
+        {
+            printTree(tree);
+            break;
+        }
+        }
     }
-
-    cout << endl;
-
-    removeElement(tree, 7);
-    removeElement(tree, 9);
-    removeElement(tree, 6);
-    removeElement(tree, 2);
-
-    printTree(tree);
-    printTreeAscending(tree);
-    printTreeDescending(tree);
-
-    for (int i = 0; i < 12; i++)
-    {
-        cout << i << ' ' << (belongs(tree, i) ? "belongs" : "doesn't belong")<< " to the set" << endl;
-    }
+    while (input);
 
     deleteTree(tree);
 
