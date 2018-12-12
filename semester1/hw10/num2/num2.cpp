0535a2df475b48858623b4b8da1dfd9cd30e0a0f
Artem Chernikov <artem_chernikov00@list.ru>
2018-12-13 01:36:16 +0300

    Added task 10.2

---
+++ semester1/hw10/num2/num2.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int length(char *string);
+bool isEqual(char *firstString, char *secondString);
+
+int main()
+{
+    const char fileName[] = "file.txt";
+    const int modulo = 10007;
+    const int p = 7;
+    const int maxLength = 256;
+
+    cout << "Enter the pattern: ";
+    char *pattern = new char[maxLength] {};
+    gets(pattern);
+
+    int patternLength = length(pattern);
+
+    int pForUsableCalculation = 1;
+    int patternHash = 0;
+    for (int i = 0; i < patternLength; i++)
+    {
+        patternHash = ((patternHash * p) % modulo + pattern[i]) % modulo;
+        pForUsableCalculation *= p;
+    }
+    pForUsableCalculation /= p;
+
+    ifstream fin(fileName);
+
+    int hash = 0;
+    char *substring = new char[patternLength] {};
+    for (int i = 0; i < patternLength; i++)
+    {
+        fin.get(substring[i]);
+        hash = ((hash * p) % modulo + substring[i]) % modulo;
+    }
+    substring[patternLength] = '\0';
+
+    int index = 0;
+    do
+    {
+        if (hash == patternHash && isEqual(pattern, substring))
+        {
+            cout << index << endl;
+        }
+
+        hash = ((hash - (substring[0] * pForUsableCalculation) % modulo) * p) % modulo;
+        if (hash < 0)
+        {
+            hash += modulo;
+        }
+
+        for (int i = 0; i < patternLength - 1; i++)
+        {
+            swap(substring[i], substring[i + 1]);
+        }
+
+        fin.get(substring[patternLength - 1]);
+        hash = (hash + substring[patternLength - 1]) % modulo;
+        index++;
+    }
+    while (!fin.eof());
+
+    fin.close();
+
+    delete[] substring;
+    delete[] pattern;
+
+    return 0;
+}
+
+int length(char *string)
+{
+    int result = 0;
+    while (string[result++] != '\0');
+
+    return result - 1;
+}
+
+bool isEqual(char *firstString, char *secondString)
+{
+    if (length(firstString) != length(secondString))
+    {
+        return false;
+    }
+
+    for (int i = 0; i < length(firstString); i++)
+    {
+        if (firstString[i] != secondString[i])
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
