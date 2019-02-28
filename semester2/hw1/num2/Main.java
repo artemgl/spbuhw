883244715b9d5248f410cce1fd31b7779ebf3829
Artem Chernikov <artem_chernikov00@list.ru>
2019-03-01 01:17:18 +0300

    Added interactive mode

--- semester2/hw1/num2/Main.java
+++ semester2/hw1/num2/Main.java
@@ -2,18 +2,43 @@ package com.artemchernikov.g144;
 
 public class Main {
     public static void main(String[] argc) {
-        List MyList = new List();
 
-        for (int i = 0; i < 10; i++) {
-            MyList.addElement(i + 1);
-        }
+        System.out.println("Enter:");
+        System.out.println("0 - exit");
+        System.out.println("1 - add the value");
+        System.out.println("2 - remove the value");
+        System.out.println("3 - check if the value exists in the list");
+        System.out.println("4 - print the list");
 
-        for (int i = 0; i < 10; i += 2) {
-            MyList.removeElement(i + 1);
-        }
+        Stack NewList = new List();
 
-        System.out.println(MyList.exists(7));
-
-        MyList.print();
+        Scanner in = new Scanner(System.in);
+        int input = 0;
+        do {
+            input = in.nextInt();
+            switch (input) {
+                case 1:
+                    System.out.println("Enter the value to add");
+                    int value = in.nextInt();
+                    NewList.addElement(value);
+                    break;
+                case 2:
+                    System.out.println("Enter the value to remove");
+                    int value = in.nextInt();
+                    NewList.removeElement(value);
+                    break;
+                case 3:
+                    System.out.println("Enter the value");
+                    int value = in.nextInt();
+                    System.out.println("This value" + (NewList.exists(value) ? "" : " doesn't") + " exists in the list");
+                    break;
+                case 4:
+                    NewList.print();
+                    break;
+                default:
+                    input = 0;
+                    break;
+            }
+        } while (input != 0);
     }
 }
