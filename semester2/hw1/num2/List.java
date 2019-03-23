c96abb407fde14f84e38b2d6d9aac5ca6db9cd40
Artem Chernikov <artem_chernikov00@list.ru>
2019-03-23 14:06:33 +0300

    Class ListElement moved to class List

--- semester2/hw1/num2/List.java
+++ semester2/hw1/num2/List.java
@@ -3,6 +3,17 @@ package com.artemchernikov.g144;
 /**A class describing singly linked list*/
 public class List {
 
+    /**A class describing element of singly linked list*/
+    public class ListElement {
+        public ListElement(int value, ListElement next) {
+            this.value = value;
+            this.next = next;
+        }
+
+        public int value;
+        public ListElement next;
+    }
+
     private ListElement first;
 
     public void addElement(int value) {
