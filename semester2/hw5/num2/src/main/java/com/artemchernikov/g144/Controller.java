a333f05d17c18c8c18ef233f6f175bc47a966a4e
Artem Chernikov <artem_chernikov00@list.ru>
2019-04-26 23:40:53 +0300

    Optimized choicebox realization and event handling

--- semester2/hw5/num2/src/main/java/com/artemchernikov/g144/Controller.java
+++ semester2/hw5/num2/src/main/java/com/artemchernikov/g144/Controller.java
@@ -1,22 +1,11 @@
 package com.artemchernikov.g144;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
 
 public class Controller {
-
-    private ObservableList<Character> operationList = FXCollections.observableArrayList('+', '-', '*', '/');
-
-    @FXML
-    private void initialize() {
-        operation.setValue('+');
-        operation.setItems(operationList);
-    }
 
     @FXML
     private TextField result;
@@ -28,24 +17,25 @@ public class Controller {
     private Spinner secondOperand;
 
     @FXML
-    private ChoiceBox<Character> operation;
+    private ChoiceBox<String> operation;
 
+    /**A method calculating value of the expression composed by user*/
     public void calculate() {
         double answer = 0;
-        char operator = operation.getValue();
+        String operator = operation.getValue();
         double first = (Integer)firstOperand.getValue();
         double second = (Integer)secondOperand.getValue();
         switch (operator) {
-            case '+':
+            case "+":
                 answer = first + second;
                 break;
-            case '-':
+            case "-":
                 answer = first - second;
                 break;
-            case '*':
+            case "*":
                 answer = first * second;
                 break;
-            case '/':
+            case "/":
                 answer = first / second;
                 break;
         }
