588671d2d5db489de1655d378818128bc0652659
Artem Chernikov <artem_chernikov00@list.ru>
2019-05-12 00:09:15 +0300

    Added zoom

--- semester2/hw5/num2/src/main/java/com/artemchernikov/g144/Main.java
+++ semester2/hw5/num2/src/main/java/com/artemchernikov/g144/Main.java
@@ -21,7 +21,8 @@ public class Main extends Application {
             root = FXMLLoader.load(getClass().getClassLoader().getResource("main.fxml"));
             Scene scene = new Scene(root);
             primaryStage.setTitle("Calculator");
-            primaryStage.setResizable(false);
+            primaryStage.setMinWidth(400);
+            primaryStage.setMinHeight(100);
             primaryStage.setScene(scene);
             primaryStage.show();
         } catch (IOException exc) {
