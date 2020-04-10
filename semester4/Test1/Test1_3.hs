fdfdca0ccd16824fdf4b8ce1793e2df93b0b2a03
Artem Chernikov <artem_chernikov00@list.ru>
2020-04-10 18:02:45 +0300

    Added test 1.3

---
+++ semester4/Test1/Test1_3.hs
@@ -0,0 +1,12 @@
+module Test1_3 where
+
+import Control.Monad
+import System.IO
+
+rhombus n = do
+    forM_ ([1,3..2*n-1] ++ reverse [1,3..2*n-3]) (\k -> do
+        forM_ [1..(2*n-1-k) `div` 2] $ const $ putStr " "
+        forM_ [1..k] $ const $ putStr "X"
+        forM_ [1..(2*n-1-k) `div` 2] $ const $ putStr " "
+        putStrLn "")
+    
