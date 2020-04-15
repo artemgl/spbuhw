d8e27eab61bd362be7a602730af282733bd52b61
Artem Chernikov <artem_chernikov00@list.ru>
2020-04-16 00:08:36 +0300

    Solution is simplified

--- semester4/hw3/Task3_2.hs
+++ semester4/hw3/Task3_2.hs
@@ -2,14 +2,4 @@ module Task3_2 where
 
 import Prelude
 
-ost = 1 : map next' ost 
-    where
-        next' 1 = 7
-        next' 7 = 9
-        next' 9 = 11
-        next' x = case (m) of
-                    9 -> 1 + 10 * next' d
-                    _ -> 10 * d + next' m
-                  where
-                    d = div x 10
-                    m = mod x 10
+osn = 1 : 7 : 9 : concatMap (\x -> map (10*x+) [1,7,9]) osn
