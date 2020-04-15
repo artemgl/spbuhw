a7e8903ef6cf0f07fabc453845c849ac334552dd
Artem Chernikov <artem_chernikov00@list.ru>
2020-04-16 02:44:33 +0300

    Optimised solution

--- semester4/hw4/Task4_1.hs
+++ semester4/hw4/Task4_1.hs
@@ -1,10 +1,10 @@
-module Task3_4 where
+module Task4_1 where
 
 import Prelude
 
 sumOfEven1 = sum . map (\x -> 1 - x `mod` 2)
 
-sumOfEven2 l = sum [1 | x <- filter (\x -> x `mod` 2 == 0) l]
+sumOfEven2 = length . filter (\x -> x `mod` 2 == 0)
 
-sumOfEven3 :: [Int] -> Int
+sumOfEven3 :: Integral a => [a] -> a
 sumOfEven3 = foldr (\x y -> y + 1 - x `mod` 2) 0
