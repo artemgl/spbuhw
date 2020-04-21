3b6268d7ee501e7a05c39f554071d09f8e097492
Artem Chernikov <artem_chernikov00@list.ru>
2020-04-21 11:30:56 +0300

    Added task 6.2

---
+++ semester4/hw6/Task6_2.hs
@@ -0,0 +1,98 @@
+module Task6_2 where
+
+import System.Random
+import Control.Monad.State
+import Data.List(sort)
+
+data BST a = Null | Branch (BST a) a (BST a)
+    deriving (Show)
+
+instance Foldable BST where
+    foldr _ b Null = b
+    foldr f b (Branch tL a tR) = foldr f (f a $ foldr f b tR) tL
+
+size :: BST a -> Int
+size = foldl (const.(+1)) 0
+
+insert :: Ord a => a -> BST a -> BST a
+insert e Null = Branch Null e Null
+insert e tree@(Branch tL a tR)
+    | e < a = Branch (insert e tL) a tR
+    | e > a = Branch tL a (insert e tR)
+    | otherwise = tree
+
+remove :: Ord a => a -> BST a -> BST a
+remove _ Null = Null
+remove e (Branch tL a tR)
+    | e < a = Branch (remove e tL) a tR
+    | e > a = Branch tL a (remove e tR)
+    | otherwise = case (tL, tR) of
+        (Null, _) -> tR
+        (_, Null) -> tL
+        _ ->
+            let
+                (a', tL') = popMax tL
+                popMax (Branch left el Null) = (el, left)
+                popMax (Branch left el right) = (fst pr, Branch left el (snd pr)) where
+                    pr = popMax right
+            in Branch tL' a' tR
+        
+
+exists :: Ord a => a -> BST a -> Bool
+exists e Null = False
+exists e (Branch tL a tR)
+    | e == a = True
+    | e < a = exists e tL
+    | otherwise = exists e tR
+
+height :: BST a -> Int
+height Null = 0
+height (Branch tL a tR) = 1 + max (height tL) (height tR)
+
+getAny :: Random a => State StdGen a
+getAny = do
+    g <- get
+    (x,g') <- return $ random g
+    put g'
+    return x
+
+getOne :: Random a => (a,a) -> State StdGen a
+getOne bounds = do
+    g <- get
+    (x,g') <- return $ randomR bounds g
+    put g'
+    return x
+
+getAnyTree :: Random a => BST a -> State StdGen (BST a)
+getAnyTree Null = return Null
+getAnyTree (Branch tL a tR) = do
+    tL' <- getAnyTree tL
+    a' <- getAny
+    tR' <- getAnyTree tR
+    return $ Branch tL' a' tR'
+
+getOneTree :: Random a => (a,a) -> BST a -> State StdGen (BST a)
+getOneTree bounds Null = return Null
+getOneTree bounds (Branch tL a tR) = do
+    tL' <- getOneTree bounds tL
+    a' <- getOne bounds
+    tR' <- getOneTree bounds tR
+    return $ Branch tL' a' tR'
+
+randomize :: Random a => BST a -> StdGen -> BST a
+randomize = evalState . getAnyTree
+
+randomizeR :: Random a => (a, a) -> BST a -> StdGen -> BST a
+randomizeR = (evalState .) . getOneTree
+
+fromList :: Ord a => [a] -> BST a
+fromList =
+    let
+        fromList' [] = Null
+        fromList' list = Branch (fromList left) mid (fromList right) where
+            left = take ((n - 1) `div` 2) list
+            right = drop midIndex list
+            mid = head $ drop (midIndex - 1) list
+            midIndex = (n + 1) `div` 2
+            n = length list
+    in fromList' . sort
