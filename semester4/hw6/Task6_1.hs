2ea3dc2f173f5f5faad419fdd9643ab5afe60c01
Artem Chernikov <artem_chernikov00@list.ru>
2020-04-27 20:21:08 +0300

    Advanced implementation of show

--- semester4/hw6/Task6_1.hs
+++ semester4/hw6/Task6_1.hs
@@ -2,11 +2,32 @@ module Task6_1 where
 
 data Polynom a = Null | a :+: Polynom a
 
-instance Show a => Show (Polynom a) where
-    show Null = ""
-    show (a:+:p) = show a ++ show' p 1 where
-        show' Null _ = ""
-        show' (a:+:p) n = " + " ++ show a ++ " * x^" ++ show n ++ show' p (n+1)
+instance (Num a, Ord a, Show a) => Show (Polynom a) where
+    show p = showHelper p 0 where
+        showHelper Null _ = ""
+        showHelper (a:+:p) n
+            | a == fromInteger 0 = showHelper p (n + 1)
+            | otherwise = showFirst a n ++ showTail p (n + 1) where
+            showFirst a n
+                | n == 0 = show a
+                | a < fromInteger 0 = "-" ++ showCoeff a ++ showDeg n
+                | otherwise = showCoeff a ++ showDeg n
+            showTail Null _ = ""
+            showTail (a:+:p) n = showSummand a n ++ showTail p (n + 1)
+            showSummand a n
+                | a == fromInteger 0 = ""
+                | otherwise = showSign a ++ showCoeff a ++ showDeg n
+            showSign a
+                | a < fromInteger 0 = " - "
+                | otherwise = " + "
+            showCoeff a
+                | a == fromInteger (-1) || a == fromInteger 0 || a == fromInteger 1 = ""
+                | a < fromInteger 0 = show (negate a)
+                | otherwise = show a
+            showDeg n
+                | n == 0 = ""
+                | n == 1 = "x"
+                | otherwise = "x^" ++ show n
 
 instance Num a => Num (Polynom a) where
     f + Null = f
