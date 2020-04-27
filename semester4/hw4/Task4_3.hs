9d60ad5ccd6cacf50340aaf54916e5029ab61a4d
Artem Chernikov <artem_chernikov00@list.ru>
2020-04-27 12:12:05 +0300

    Added test

--- semester4/hw4/Task4_3.hs
+++ semester4/hw4/Task4_3.hs
@@ -1,12 +1,15 @@
 module Task4_3 where
 
-data Tree a = Null | Leaf a | Branch (Tree a) a (Tree a)
+data Tree a = Null | Branch (Tree a) a (Tree a)
 
 instance Foldable Tree where
     foldr f b tree = case tree of
         Null -> b
-        Leaf a -> f a b
         Branch aL a aR -> foldr f (f a $ foldr f b aR) aL
 
 getList :: Tree a -> [a]
 getList x = foldr (:) [] x
+
+tree = Branch (Branch (Branch Null 94 (Branch Null 21 Null)) 51 (Branch (Branch Null 32 (Branch Null 81 Null)) 40 Null)) 67 (Branch Null 75 (Branch (Branch Null 23 Null) 93 Null))
+
+test = getList tree
