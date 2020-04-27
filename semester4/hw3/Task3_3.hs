bbb2c294b129c545553a92ad356db5a56b3b17d8
Artem Chernikov <artem_chernikov00@list.ru>
2020-04-27 12:48:28 +0300

    Redone correctly

--- semester4/hw3/Task3_3.hs
+++ semester4/hw3/Task3_3.hs
@@ -2,16 +2,10 @@ module Task3_3 where
 
 import Prelude
 
-fstPos :: (Ord a, Num a) => [a] -> Int
-fstPos [] = error "Empty list"
-fstPos (x:[]) = 1
-fstPos (x:y:[]) = if x > y then 2 else 1
-fstPos (x:y:z:[]) = if x + z > y then 2 else 1
-fstPos x = fstPos' (head x) 0 2 x where
-                                fstPos' :: (Ord a, Num a) => a -> Int -> Int -> [a] -> Int
-                                fstPos' a n m (x:y:[]) = n
-                                fstPos' a n m (x:xs) = if sum > a
-                                                       then k sum m (m+1) xs
-                                                       else k a n (m+1) xs
-                                                       where
-                                                           sum = x + head (tail xs)
+fstPos [] = 0
+fstPos s@(x:xs) = fstPos' s x 0 0 where
+    fstPos' (a:b:xs) max index ans = let sum = a + b in
+        if sum > max
+        then fstPos' (b:xs) sum (index + 1) (index + 1)
+        else fstPos' (b:xs) max (index + 1) ans
+    fstPos' _ _ _ ans = ans
